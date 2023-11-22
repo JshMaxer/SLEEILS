2a8113f3fd249e2ecdd5cb9604aca38f3b095892
Joshua Cambal <joshuacambal01@gmail.com>
2023-11-22 10:07:00 +0800

    - Added Activation in the student enroll

--- Database/DBStudentEnroll.cs
+++ Database/DBStudentEnroll.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using Guna.UI2.WinForms;
+﻿using Guna.UI2.WinForms;
 using MySql.Data.MySqlClient;
 using SMARTLEARN.FrontEnd.Message;
 using SMARTLEARN.Model;
+using System;
+using System.Windows.Forms;
 
 namespace SMARTLEARN.Database
 {
@@ -16,20 +12,46 @@ namespace SMARTLEARN.Database
     {
         MySqlConnection connection = Host.connection;
 
-        Random ran = new Random(); //Temporary. Make this as checking the database first before entering new value
-
         //Insert Query
         public void insertinfo(Guna2ComboBox admittype, Guna2ComboBox yearlevel, Guna2ComboBox schoolyear, Guna2ComboBox term, Guna2RadioButton ICT, Guna2TextBox fname, Guna2TextBox mname, Guna2TextBox lname, Guna2TextBox suffix, Guna2ComboBox gender, Guna2ComboBox status, Guna2TextBox Citizenship, Guna2DateTimePicker birth, Guna2TextBox birthplace, Guna2TextBox religion, Guna2TextBox address, Guna2TextBox email, Guna2TextBox mobile, Guna2TextBox telephone, Guna2TextBox gfname, Guna2TextBox gmname, Guna2TextBox glname, Guna2TextBox gsuffix, Guna2ComboBox ggender, Guna2ComboBox gstatus, Guna2TextBox gCitizenship, Guna2DateTimePicker gbirth, Guna2TextBox gbirthplace, Guna2TextBox greligion, Guna2TextBox gaddress, Guna2TextBox gemail, Guna2TextBox gmobile, Guna2TextBox gtelephone, Guna2TextBox grelationship, Guna2TextBox goccupation)
         {
-            if(Backend.BEStudentEnroll.listtype == "Type of Student: New")
+            //-----------------------------------------------------------------------------------
+            //CHECK ID FOR UNIQUENESS
+
+            Random ran = new Random();
+            string iD = 2000 + ran.Next(100000, 999999).ToString();
+            
+            //Check if the ID is existing
+            string USERID = $"SELECT ID FROM studentaccount WHERE ID = '{iD}'";
+            connection.Open();
+
+            MySqlCommand cmd = new MySqlCommand(USERID, connection);
+            MySqlDataReader row = cmd.ExecuteReader();
+
+            if (row.HasRows) //If the ID is exist!
             {
-                string InsertQuery = $"INSERT INTO studentaccount VALUES ({ran.Next(100, 999999)}, 'New', '{admittype.Text}', '{yearlevel.Text}', '{schoolyear.Text}', '{term.Text}', '{ICT.Text}', '{fname.Text}', '{mname.Text}', '{lname.Text}', '{suffix.Text}', '{gender.Text}', '{status.Text}', '{Citizenship.Text}', '{birth.Value.ToString("yyyy-MM-dd")}', '{birthplace.Text}', '{religion.Text}', '{address.Text}', '{email.Text}', '{mobile.Text}', '{telephone.Text}', '{gfname.Text}', '{gmname.Text}', '{glname.Text}', '{gsuffix.Text}', '{ggender.Text}', '{gstatus.Text}', '{gCitizenship.Text}', '{gbirth.Value.ToString("yyyy-MM-dd")}', '{gbirthplace.Text}', '{gaddress.Text}', '{gemail.Text}', '{gmobile.Text}', '{gtelephone.Text}', '{grelationship.Text}', '{goccupation.Text}', '1234')";
+                while (row.Read())
+                {
+                    iD = 2000 + ran.Next(100000, 999999).ToString();
+                }
+            }
+            else
+            {
+                //No random will made
+            }
+            connection.Close();
+
+            //-----------------------------------------------------------------------------------
+
+            if (Backend.BEStudentEnroll.listtype == "Type of Student: New")
+            {
+                string InsertQuery = $"INSERT INTO studentaccount VALUES ({iD}, 'New', '{admittype.Text}', '{yearlevel.Text}', '{schoolyear.Text}', '{term.Text}', '{ICT.Text}', '{fname.Text}', '{mname.Text}', '{lname.Text}', '{suffix.Text}', '{gender.Text}', '{status.Text}', '{Citizenship.Text}', '{birth.Value.ToString("yyyy-MM-dd")}', '{birthplace.Text}', '{religion.Text}', '{address.Text}', '{email.Text}', '{mobile.Text}', '{telephone.Text}', '{gfname.Text}', '{gmname.Text}', '{glname.Text}', '{gsuffix.Text}', '{ggender.Text}', '{gstatus.Text}', '{gCitizenship.Text}', '{gbirth.Value.ToString("yyyy-MM-dd")}', '{gbirthplace.Text}', '{gaddress.Text}', '{gemail.Text}', '{gmobile.Text}', '{gtelephone.Text}', '{grelationship.Text}', '{goccupation.Text}', '1234', 'Not Activated')";
                 connection.Open();
-                MySqlCommand cmd = new MySqlCommand(InsertQuery, connection);
+                MySqlCommand cmd1 = new MySqlCommand(InsertQuery, connection);
 
                 try
                 {
-                    if (cmd.ExecuteNonQuery() == 1)
+                    if (cmd1.ExecuteNonQuery() == 1)
                     {
                         FrontEnd.ProcessMessage pm = new FrontEnd.ProcessMessage();
                         FrontEnd.ProcessMessage.message = "YOUR ENROLLMENT IS\r\nBEING PROCESS!";
@@ -50,13 +72,13 @@ namespace SMARTLEARN.Database
             }
             else
             {
-                string InsertQuery = $"INSERT INTO studentaccount VALUES ({ran.Next(100, 999999)}, 'Existing', '{admittype.Text}', '{yearlevel.Text}', '{schoolyear.Text}', '{term.Text}', '{ICT.Text}', '{fname.Text}', '{mname.Text}', '{lname.Text}', '{suffix.Text}', '{gender.Text}', '{status.Text}', '{Citizenship.Text}', '{birth.Value.ToString("yyyy-MM-dd")}', '{birthplace.Text}', '{religion.Text}', '{address.Text}', '{email.Text}', '{mobile.Text}', '{telephone.Text}', '{gfname.Text}', '{gmname.Text}', '{glname.Text}', '{gsuffix.Text}', '{ggender.Text}', '{gstatus.Text}', '{gCitizenship.Text}', '{gbirth.Value.ToString("yyyy-MM-dd")}', '{gbirthplace.Text}', '{gaddress.Text}', '{gemail.Text}', '{gmobile.Text}', '{gtelephone.Text}', '{grelationship.Text}', '{goccupation.Text}', '1234')";
+                string InsertQuery = $"INSERT INTO studentaccount VALUES ({iD}, 'Existing', '{admittype.Text}', '{yearlevel.Text}', '{schoolyear.Text}', '{term.Text}', '{ICT.Text}', '{fname.Text}', '{mname.Text}', '{lname.Text}', '{suffix.Text}', '{gender.Text}', '{status.Text}', '{Citizenship.Text}', '{birth.Value.ToString("yyyy-MM-dd")}', '{birthplace.Text}', '{religion.Text}', '{address.Text}', '{email.Text}', '{mobile.Text}', '{telephone.Text}', '{gfname.Text}', '{gmname.Text}', '{glname.Text}', '{gsuffix.Text}', '{ggender.Text}', '{gstatus.Text}', '{gCitizenship.Text}', '{gbirth.Value.ToString("yyyy-MM-dd")}', '{gbirthplace.Text}', '{gaddress.Text}', '{gemail.Text}', '{gmobile.Text}', '{gtelephone.Text}', '{grelationship.Text}', '{goccupation.Text}', '1234', 'Not Activated')";
                 connection.Open();
-                MySqlCommand cmd = new MySqlCommand(InsertQuery, connection);
+                MySqlCommand cmd2 = new MySqlCommand(InsertQuery, connection);
 
                 try
                 {
-                    if (cmd.ExecuteNonQuery() == 1)
+                    if (cmd2.ExecuteNonQuery() == 1)
                     {
                         FrontEnd.ProcessMessage pm = new FrontEnd.ProcessMessage();
                         FrontEnd.ProcessMessage.message = "YOUR ENROLLMENT IS\r\nBEING PROCESS!";
@@ -75,7 +97,6 @@ namespace SMARTLEARN.Database
                     MessageBox.Show(ex.Message);
                 }
             }
-            
 
             connection.Close();
 
--- FrontEnd/FEStudentEnroll.cs
+++ FrontEnd/FEStudentEnroll.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SMARTLEARN.Backend;
+using System;
 using System.Windows.Forms;
 
 namespace SMARTLEARN.FrontEnd
