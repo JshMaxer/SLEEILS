772c2d1e7b0b4771f42a646847ca350d1429d7b6
Joshua Cambal <joshuacambal01@gmail.com>
2023-12-05 23:03:19 +0800

    - Adjusted something. Waitings sa games ni DANI.

--- FrontEnd/MainForm/StudentForm/FESTForgotpass.Designer.cs
+++ FrontEnd/MainForm/StudentForm/FESTForgotpass.Designer.cs
@@ -101,7 +101,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Bahnschrift SemiBold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(229, 79);
+            this.label2.Location = new System.Drawing.Point(218, 79);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(79, 16);
             this.label2.TabIndex = 38;
